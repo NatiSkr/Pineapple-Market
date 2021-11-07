e1d56ecbcc3d5bd735cf504158e02499f091e4ba
Natacha Skrzyniecki <skrzyniecki.natacha@gmail.com>
2021-11-07 12:31:33 -0300

    added decorators

--- back-end/spring256market/src/main/java/com/example/spring256market/controllers/Products.java
+++
@@ -1,5 +0,0 @@
-package com.example.spring256market.controllers;
-
-public class Products {
-
-}
---
+++ back-end/spring256market/src/main/java/com/example/spring256market/controllers/UsersController.java
@@ -0,0 +1,11 @@
+package com.example.spring256market.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api") // handle only /api requests
+
+public class UsersController {
+
+}
